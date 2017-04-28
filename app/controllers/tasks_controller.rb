3c10e3a7d19b5afcb041fd0a325b92d8a5dd3fed
Tyler Braddock <tyler.braddock@yahoo.com>
2017-04-28 15:23:40 +0000

    In tasks_controller_spec.rb, wrote test for task#create, testing that new tasks can be created. In tasks_controller.rb, set up create action to create task and render a JSON response. Updated task_params to include the :title field.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,11 @@ class TasksController < ApplicationController
     render json: Task.order(:id)
   end
 
+  def create
+    task = Task.create(task_params)
+    render json: task
+  end
+
   def update
     task = Task.find(params[:id])
     task.update_attributes(task_params)
@@ -12,6 +17,6 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:done)
+    params.require(:task).permit(:done, :title)
   end
 end
