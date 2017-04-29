d92a938467e33569474256db94869f989d470326
Tyler Braddock <tyler.braddock@yahoo.com>
2017-04-29 00:02:36 +0000

    In tasks.js, added strikethrough line feature for when tasks are toggled to checked state. Updated var liElement to allow the li in the string to have a class that shows completed when the task is checked and blank when the task is unchecked. Also, added id to li element in var liElement so that each task can be uniquely identified. Updated toggleTask function to have new HTML displayed upon successful completion of update to database.

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,7 +1,8 @@
 $(function() {
   function taskHtml(task) {
     var checkedStatus = task.done ? "checked" : "";
-    var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+    var liClass = task.done ? "completed" : "";
+    var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '"><div class="view"><input class="toggle" type="checkbox"' +
       " data-id='" + task.id + "'" + checkedStatus + '><label>' + task.title +
       '</label></div></li>';
     return liElement
@@ -16,6 +17,11 @@ $(function() {
       task: {
         done: doneValue
       }
+    }).success(function(data) {
+      var liHtml = taskHtml(data);
+      var $li = $("#listItem-" + data.id);
+      $li.replaceWith(liHtml);
+      $('.toggle').change(toggleTask);
     });
   }
 
